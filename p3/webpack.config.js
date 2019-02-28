731e9ab9698aa204f78442f02699f10c61533d14
Arnar Leifsson <drleifz@gmail.com>
2019-02-28 08:17:14 +0000

    Adding missing items for the lecture today

--- p3/webpack.config.js
+++ p3/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: [ '@babel/polyfill', './src/index.js' ],
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
